f504715aeb79a832085601711fd32101874a52df
cristian-rincon <cristian.o.rincon.b@gmail.com>
2019-12-11 20:08:22 -0500

    hello flask

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask,request,make_response,redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    user_ip = request.remote_addr
+    
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip',user_ip)
+
+    return response
+
+
+@app.route('/hello')
+def hello():
+
+    user_ip = request.cookies.get('user_ip')
+
+    b = f"Hello world in Flask. Your IP is {user_ip}"
+    return b
